e004154933be9b19e48aa5d89f2a8e8fc36097d1
billzt <billzt@hotmail.com>
2015-08-30 16:15:30 +0800

    Update kseq_fastq_base.c

--- kseq_fastq_base.c
+++ kseq_fastq_base.c
@@ -4,6 +4,8 @@
 
 // reference: http://lh3lh3.users.sourceforge.net/parsefastq.shtml
 
+// Tao Zhu 2015-08-30
+
 // STEP 1: declare the type of file handler and the read() function  
 KSEQ_INIT(gzFile, gzread)  
   
@@ -16,13 +18,13 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Usage: %s <in.seq>\n", argv[0]);  
         return 1;  
     }
-    int n = 0, slen = 0, qlen = 0;
+    long int n = 0, slen = 0;
     fp = gzopen(argv[1], "r"); // STEP 2: open the file handler  
     seq = kseq_init(fp); // STEP 3: initialize seq  
     while ((l = kseq_read(seq)) >= 0) { // STEP 4: read sequence  
         ++n, slen += seq->seq.l;
     }  
-    printf("Num reads:%d\tNum Bases: %d\n", n, slen); 
+    printf("Num reads:%ld\tNum Bases: %ld\n", n, slen); 
     kseq_destroy(seq); // STEP 5: destroy seq  
     gzclose(fp); // STEP 6: close the file handler  
     return 0;  
